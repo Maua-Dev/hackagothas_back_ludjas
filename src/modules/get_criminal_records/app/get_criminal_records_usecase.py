9493c81696f94a5e1514711f9a314bdfaba0b704
Luigi Trevisan <checkzin123@gmail.com>
2023-03-22 17:27:49 -0300

    usecase + TU 🔫💼

---
+++ src/modules/get_criminal_records/app/get_criminal_records_usecase.py
@@ -0,0 +1,21 @@
+from src.shared.domain.repositories.criminal_record_repository_interface import ICriminalRecordRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+
+
+class GetCriminalRecordsUsecase:
+    def __init__(self, repo: ICriminalRecordRepository):
+        self.repo = repo
+        
+    def __call__(self, criminal_record_id: int):
+        if type(criminal_record_id) != int:
+            raise EntityError('criminal_record_id')
+        if criminal_record_id < 0:
+            raise EntityError('criminal_record_id')
+
+        criminal_record = self.repo.get_criminal_records(criminal_record_id)
+        
+        if criminal_record == None:
+            raise NoItemsFound('criminal_record_id')
+        
+        return criminal_record
