72451fc5daaf1eb13cc899a114961417eab3f5ad
Luigi Trevisan <checkzin123@gmail.com>
2023-03-24 10:13:45 -0300

    environments repo fix 🐒♻️

--- src/modules/get_criminal_record/app/get_criminal_record_presenter.py
+++ src/modules/get_criminal_record/app/get_criminal_record_presenter.py
@@ -1,13 +1,12 @@
+from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 from .get_criminal_record_controller import GetCriminalRecordController
 from .get_criminal_record_usecase import GetCriminalRecordUsecase
-from src.shared.infra.repositories.criminal_record_repository_mock import CriminalRecordRepositoryMock
 
-
+repo = Environments.get_criminal_record_repo()()
+usecase = GetCriminalRecordUsecase(repo=repo)
+controller = GetCriminalRecordController(usecase=usecase)
 def lambda_handler(event, context):
-    repo = CriminalRecordRepositoryMock()
-    usecase = GetCriminalRecordUsecase(repo)
-    controller = GetCriminalRecordController(usecase)
     
     httpRequest = LambdaHttpRequest(data=event)
     response = controller(httpRequest)
